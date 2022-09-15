c875cf707e0d3cfe70d713de6b65a675f1bb09df
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2022-09-15 19:59:16 +1200

    pokemon_card

---
+++ components/cards/pokes/PokeCard.mocks.ts
@@ -0,0 +1,13 @@
+import { IPokeCard } from './PokeCard';
+
+const base: IPokeCard = {
+  tag: 'Felines',
+  title: `What's new in Cats`,
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  author: 'Alex',
+  time: '2h ago',
+};
+
+export const mockPokeCardProps = {
+  base,
+};
---
+++ components/cards/pokes/PokeCard.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import PokeCard, { IPokeCard } from './PokeCard';
+import { mockPokeCardProps } from './PokeCard.mocks';
+
+export default {
+  title: 'templates/PokeCard',
+  component: PokeCard,
+  argTypes: {},
+} as ComponentMeta<typeof PokeCard>;
+
+const Template: ComponentStory<typeof PokeCard> = (args) => (
+  <PokeCard {...args} />
+);
+
+export const Base = Template.bind({});
+
+Base.args = {
+  ...mockPokeCardProps.base,
+} as IPokeCard;
