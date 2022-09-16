e1f61ef36eaf918aaf6c3cb2739941c7544803bf
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2022-09-16 13:36:06 +1200

    second rebuild

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['i.pravatar.cc', 'raw.githubusercontent.com'],
-  }
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
