47b21a413f7f0813b910a86823a8e067d164bf72
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2022-09-16 20:49:22 +1200

    final addons

--- stories/PokeCard.jsx
+++ stories/PokeCard.jsx
@@ -2,9 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Image from 'next/image';
 
-export default function PokeCard({ name, url, defense }) {
+export default function PokeCard({ name, url, defense , backgroundColor, handleClick}) {
+  const styleCard = {
+    backgroundColor,
+    border: "none",
+    width: "clamp(10rem, calc(10rem + 2vw), 15rem)",
+    boxShadow: "0 0.1rem 1rem rgba(0, 0, 0, 0.1)",
+    borderRadius: "1em",
+    display: "flex",
+    flexDirection: "column",
+    fontFamily: "'Quicksand', sans-serif",
+    overflow: "hidden"
+  }
+  const styleHeader = {
+    fontSize: "1.5rem",
+    textTransform: "capitalize"
+  }
+  const styleOutCard = {
+      padding: "1.5rem",
+      textAlign: "center",
+      textDecoration: "none",
+      maxWidth: "300px"
+  }
   return (
-    <div className="container">
+    <div className="outer-container" style={styleOutCard}>
+    <div className="container" style={styleCard}>
       <div className="card" >
         <div className="card-header">
           <Image
@@ -16,10 +38,11 @@ export default function PokeCard({ name, url, defense }) {
           />
         </div>
         <div className="card-body">
-          <h3>{name}</h3>
+          <h3 style={styleHeader}>{name}</h3>
           <p>{defense}</p>
         </div>
       </div>
+    </div>
     </div>
   );
 }
--- stories/PokeCard.stories.jsx
+++ stories/PokeCard.stories.jsx
@@ -1,9 +1,11 @@
 import PokeCard from './PokeCard';
 import React from 'react';
+import {within, userEvent} from "@storybook/testing-library"
 
 export default {
   title: 'PokeCard',
-  component: PokeCard
+  component: PokeCard,
+  argTypes: { handleClick: { action: "handleClick" } }
 } 
 
 const Template = (args) => <PokeCard {...args} />;
@@ -12,6 +14,13 @@ export const Base = Template.bind({});
 Base.args = {
   name: "ditto",
   url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg",
-  defense: "electric"
+  defense: "electric",
+  backgroundColor: "blue"
 }; 
+/*
+Base.play = async({canvasElement})=>{
+    const canvas = within(canvasElement);
+    const deleteInteraction = await canvas.getByRole('button');
+    await userEvent.click(deleteInteraction)
+}*/
 
